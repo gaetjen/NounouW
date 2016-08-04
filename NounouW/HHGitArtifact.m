6b29edd2c8fa1c665005a8babcdef1a361002663
Kenta Takagaki <kentaroh.takagaki@gmail.com>
2016-08-04 11:10:12 +0200

    Finished documentation refactor for Dominik

--- NounouW/HHGitArtifact.m
+++ NounouW/HHGitArtifact.m
@@ -1,5 +1,5 @@
 (* Created with the Wolfram Language : www.wolfram.com *)
 <|"GitPath" -> "C:\\prog\\_w\\NounouW\\.git", "GitBranch" -> "dev", 
- "GitHEAD" -> "96e68a3ca52813e37539d5a6ca66a53acb0fc06c", 
+ "GitHEAD" -> "c6fa48445eaa327d13d379f0ef7b3853f71ee9ea", 
  "GitRemotes" -> {"origin" -> "https://github.com/ktakagaki/NounouW.git"}, 
- "GitArtifactDate" -> "Tue 2 Aug 2016 09:04:35"|>
+ "GitArtifactDate" -> "Thu 4 Aug 2016 11:01:21"|>
